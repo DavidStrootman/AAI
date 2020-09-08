bf2699d3890020191c87452c9ea23fd6b5e25122
David <david.strootman@gmail.com>
2020-09-08 11:47:32 +0200

    Cleanup

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import numpy as np
 from typing import List, Optional, Tuple
 from collections import Counter
-import operator
-import math
+
 
 def import_data(filename: str, year: int) -> Tuple[np.ndarray, List[str]]:
     data: np.ndarray = np.genfromtxt(filename, delimiter=';', usecols=[1, 2, 3, 4, 5, 6, 7],
@@ -25,25 +24,35 @@ def import_data(filename: str, year: int) -> Tuple[np.ndarray, List[str]]:
 
     return data, labels
 
-# Classifier algorithm that looks in the range of K nearest neighbours to determine what classification test_point is
-def k_nearest_neighbour(k: int, test_point: List[int], training_set: np.ndarray, labels):
-    data_point_list: List[Tuple[int, str]] = list((float('inf'), "") for _ in range(k))  # TODO: create empty datapoints or something to compare to
+
+def k_nearest_neighbour(k: int, t_point: List[int], training_set: np.ndarray, labels: List[str]):
+    """calculates the k nearest neighbour points to t_point from training_set
+
+    Args:
+        k (int):  nearest neighbour count
+        t_point (List[int]): a list of features that form a data point
+        training_set (np.ndarray): the training set from which to find the nearest points
+        labels (List[str]): list of labels to assign to t_point from
+
+    Returns:
+        str: returns the classification of the t_point as a string
+    """
+    data_point_list: List[Tuple[int, str]] = list((float('inf'), "") for _ in range(k))
 
     for i, data_point in enumerate(training_set):
         distance: int = 0
         # calculate score for datapoint
         for j, feature in enumerate(data_point):
-            distance += pow(feature - test_point[j], 2)
+            distance += pow(feature - t_point[j], 2)
         for j, val in enumerate(data_point_list):
             if distance < val[0]:
                 data_point_list[j] = (distance, labels[i])
                 break
-        
-    output_list = []
+
+    output_list: List = []
     for item in data_point_list:
         output_list.append(item[1])
-        
-        
+
     count = Counter(output_list)
     if len(count) > 1:
         most_common = count.most_common(2)[0][0]
@@ -51,48 +60,81 @@ def k_nearest_neighbour(k: int, test_point: List[int], training_set: np.ndarray,
     return count.most_common(1)[0][0]
 
 
-def normalize_features(data_set, input_values=None) -> List[int]:
-    max_values = []
+def normalize_features(d_set: np.ndarray, input_values: Optional[List[float]] = None) -> List[float]:
+    """ normalize any data set to a percentage range (0-100)
+    if input_values is specified, it will use these values as the maximum of the range
 
-    for i in range(len(data_set[0])):  # first data point
-        feature_max = 0
-        for data_point in data_set:
+    Args:
+        d_set (np.ndarray): data set to normalize
+        input_values (List[float], optional): optional list of maximums to overwrite data_set maximum values.
+            Defaults to None.
+
+    Returns:
+        List[float]: list of feature maximum values
+    """
+    max_values: List[float] = []
+
+    for i in range(len(d_set[0])):  # first data point
+        feature_max: float = 0
+        for data_point in d_set:
             value = data_point[i]
             feature_max = value if value > feature_max else feature_max
         if input_values:
             feature_max = input_values[i]
-        for data_point in data_set:
+        for data_point in d_set:
             data_point[i] *= 100 / feature_max
         max_values.append(feature_max)
     return max_values
 
 
-if __name__ == '__main__':
-    data_set, data_labels = import_data('dataset1.csv', 2000)
-    validation_set, validation_labels = import_data('validation1.csv', 2001)
-    days_set, _ = import_data('days.csv', 0)
-    highest_k = 0
-    highest_value = 0
+def find_best_k(v_set: np.ndarray, v_labels: List[str], t_set: np.ndarray, t_labels: List[str]):
+    """find the best k in range of validation set
 
-    normalize_range = normalize_features(data_set)
-    normalize_features(validation_set, normalize_range)
-    normalize_features(days_set, normalize_range)
+    Args:
+        v_set (np.ndarray): array of points to validate
+        v_labels (List[str]): labels for the validation set
+        t_set (np.ndarray): array of points to test against
+        t_labels (List[str]): labels for the test set
 
-    for k in range (1, len(validation_set)):
+    Returns:
+        int: highest k value found
+    """
+    highest_k: int = 0
+    highest_value: float = 0
+    for k in range(1, len(v_set)):
         classifications = []
-        for i in range(len(validation_set)):
-            classifications.append(k_nearest_neighbour(k=k, test_point=validation_set[i], training_set=data_set, labels=data_labels))
+        for i in range(len(v_set)):
+            classifications.append(k_nearest_neighbour(k, v_set[i],
+                                                       t_set, t_labels))
 
-        matches = 0
+        matches: int = 0
         for i, c in enumerate(classifications):
-            if classifications[i] == validation_labels[i]:
+            if classifications[i] == v_labels[i]:
                 matches += 1
-        res = matches * 100 / len(validation_labels) 
+        res: float = matches * 100 / len(v_labels)
         if res > highest_value:
             highest_value = res
             highest_k = k
     print("k:", highest_k, " value:", int(highest_value), "%")
-    out = []
+    return highest_k
+
+
+if __name__ == '__main__':
+    # import data sets
+    data_set, data_labels = import_data('dataset1.csv', 2000)
+    validation_set, validation_labels = import_data('validation1.csv', 2001)
+    days_set, _ = import_data('days.csv', 0)
+
+    # normalize data
+    normalize_range: List[float] = normalize_features(data_set)
+    normalize_features(validation_set, normalize_range)
+    normalize_features(days_set, normalize_range)
+
+    # find best k
+    best_k: int = find_best_k(validation_set, validation_labels, data_set, data_labels)
+
+    # classify days set
+    out: List[str] = []
     for test_point in days_set:
-        out.append(k_nearest_neighbour(highest_k, test_point, data_set, data_labels))
+        out.append(k_nearest_neighbour(best_k, test_point, data_set, data_labels))
     print(out)
