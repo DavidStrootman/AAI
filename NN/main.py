920533b5a574fff671c7d543da72e30bbd56bc9c
Peter Schenkels <peter.schenkels@student.hu.nl>
2020-09-28 21:35:37 +0200

    Update main.py
    schiet af en toe wel erg uit

--- NN/main.py
+++ NN/main.py
@@ -14,20 +14,19 @@ class Input:
 
     def get_delta_weights_next_layer(self):
         return 0
+
 class Neuron:
 
     def __init__(self, previous_layer: list, next_layer: list, last_layer: bool):
-        self.weights = weights
-        self.bias = 0
+        self.bias = get_random()
+        self.weights = []
         self.a = 0
-        self.wij = []
         self.delta = 0
         self.last = last_layer
-        self.outputs = []
         self.previous_layer = previous_layer
         self.next_layer = next_layer
 
-        for i in len(self.previous_layer):
+        for i in range(len(self.previous_layer)):
             self.weights.append(get_random())
 
     def calculate_a(self):
@@ -51,26 +50,66 @@ class Neuron:
             input_i.delta = input_i.a * input_i.get_delta_weights_next_layer()
 
     def update_weights(self, learning_constant: float):
-        if self.last:
-            self.calculate_last()
         self.back_propegation()
         for i, input_i in enumerate(self.previous_layer):
             self.weights[i] += learning_constant * self.delta * input_i.a
-        self.bias += self.delta_j * learning_constant
+        self.bias += self.delta * learning_constant
 
 
 def get_random():
-    return random.random(0, 100) /100
+    return random.randrange(-100, 100, 1) /100
+
+
+def feed_forward_layer(layer: list):
+    for neuron in layer:
+        neuron.calculate_a()
+
+def update_weights_layer(layer: list, learning_constant: float):
+    for neuron in layer:
+        neuron.update_weights(learning_constant)
+
+def update_last_layer(last_layer: list, y_list: list):
+        for i, neuron in enumerate(last_layer):
+            neuron.calculate_last(y_list[i])
+
 
 
 if __name__ == "__main__":
+    
     first_layer = []
     hidden_layer_1 = []
     hidden_layer_2 = []
     last_layer = []
 
-    first_layer = [Input(0.1), Input(0.5)]
-    hidden_layer_1 = [Neuron(first_layer, hidden_layer_2, False), Neuron(first_layer, hidden_layer_2, False), Neuron(first_layer, hidden_layer_2, False)]
-    hidden_layer_2 = [Neuron(hidden_layer_1, last_layer, False), Neuron(hidden_layer_1, last_layer, False), Neuron(hidden_layer_1, last_layer, False), Neuron(hidden_layer_1, last_layer, False)]
-    last_layer = [Neuron(hidden_layer_2, None, True), Neuron(hidden_layer_2, None, True), Neuron(hidden_layer_2, None, True)]
+    first_layer.append(Input(0.0))
+    first_layer.append(Input(0.0))
+
+    hidden_layer_1.append(Neuron(first_layer, hidden_layer_2, False))
+    hidden_layer_1.append(Neuron(first_layer, hidden_layer_2, False))
+    hidden_layer_1.append(Neuron(first_layer, hidden_layer_2, False))
+
+    hidden_layer_2.append(Neuron(hidden_layer_1, last_layer, False)) 
+    hidden_layer_2.append(Neuron(hidden_layer_1, last_layer, False)) 
+    hidden_layer_2.append(Neuron(hidden_layer_1, last_layer, False)) 
+    hidden_layer_2.append(Neuron(hidden_layer_1, last_layer, False))
+    
+    last_layer.append(Neuron(hidden_layer_2, None, True))
+    last_layer.append(Neuron(hidden_layer_2, None, True))
+    last_layer.append(Neuron(hidden_layer_2, None, True))
+
+
+
+    correct_output = [1, 0, 1]
+    for i in range(20):
+        first_layer = [Input(0.1), Input(0.0)]
+        feed_forward_layer(hidden_layer_1)
+        feed_forward_layer(hidden_layer_2)
+        feed_forward_layer(last_layer)
+        update_last_layer(last_layer, correct_output)
+        update_weights_layer(last_layer, 0.01)
+        update_weights_layer(hidden_layer_2, 0.01)
+        update_weights_layer(hidden_layer_1, 0.01)
+        for i in last_layer:
+            print(i.a)
+        print("=======")
 
