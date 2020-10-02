fe8bbec4c0ff68a2324fc0e9234adea6cc3ab01a
Peter Schenkels <peter.schenkels@student.hu.nl>
2020-10-02 16:49:32 +0200

    epic gode!

--- NN/main.py
+++ NN/main.py
@@ -2,6 +2,9 @@ import math
 import random
 import csv
 
+
+
+#afgeldien is a(1-a)
 def sigmoid(x):
   return 1 / (1 + math.e **-x)
 
@@ -19,9 +22,10 @@ class Input:
 class Neuron:
 
     def __init__(self, previous_layer: list, next_layer: list):
-        self.bias = get_random()
+        self.bias = 0.5
         self.weights = []
         self.a = 0
+        self.z = 0
         self.delta = 0
         self.previous_layer = previous_layer
         self.next_layer = next_layer
@@ -31,13 +35,14 @@ class Neuron:
 
     def update_a(self):
         self.a = 0
+        self.z = 0
         for i, input_i in enumerate(self.previous_layer):
-            self.a += self.weights[i] * input_i.a + self.bias
-        self.a = sigmoid(self.a)
+            self.z += self.weights[i] * input_i.a + self.bias
+        self.a = sigmoid(self.z)
 
     def calculate_last(self, y_value):
-        self.update_a()
-        self.delta = self.a * (y_value - self.a)
+        self.update_a() 
+        self.delta = (sigmoid(self.z) * (1 - sigmoid(self.z))) * (y_value - self.a)
         return (y_value - self.a)
 
     def get_delta_weights_next_layer(self):
@@ -49,7 +54,7 @@ class Neuron:
 
     def back_propegation(self):
         if self.next_layer is not None:
-            self.delta = self.a * self.get_delta_weights_next_layer()
+            self.delta = (sigmoid(self.z) * (1 - sigmoid(self.z))) * self.get_delta_weights_next_layer()
 
     def update_weights(self, learning_constant: float):
         self.back_propegation()
@@ -89,8 +94,8 @@ def train_network(input_layer, hidden_layers, last_layer, learning_constant, cor
         for hidden_layer in hidden_layers:
             update_weights_layer(hidden_layer, learning_constant)
     return c 
-    # for i in last_layer:
-    #     print(i.a)
+    for i in last_layer:
+        print(i.a)
 
 
 def set_input_layer(first_layer: list, input_data: list):
@@ -154,10 +159,11 @@ def convert_to_output(index: int, list_size: int):
             output.append(0)
     return output
 
-def print_weights(layer):
-    print("======================")
-    for node in layer:
-        print(node.weights)
+def print_weights(layers):
+    for layer in layers:
+        print("======================")
+        for node in layer:
+            print(node.weights)
 
 def feed_forward_result(attributes, first_layer, hidden_layers: list, last_layer):
     set_input_layer(first_layer, attributes)
@@ -166,7 +172,7 @@ def feed_forward_result(attributes, first_layer, hidden_layers: list, last_layer
     feed_forward_layer(last_layer)
     results = []
     for neuron in last_layer:
-        results.append(neuron.a)
+        results.append(int(neuron.a >= 0.5))
     return results
     
 if __name__ == "__main__":
@@ -180,50 +186,65 @@ if __name__ == "__main__":
     data_set = import_data_set()
     data_set = normalize_data(data_set)
     #add input layer neurons for every attribute
-    first_layer.append(Input(0.0))
-    first_layer.append(Input(0.0))
-    first_layer.append(Input(0.0))
-    first_layer.append(Input(0.0))
+    for nr_first_layers in range(4):
+        first_layer.append(Input(0.0))
 
     #add hidden layer neurons
-    for nr_hidden_layers in range(2):
+    for nr_hidden_layers in range(4):
         hidden_layer_1.append(Neuron(first_layer, hidden_layer_2))
 
     #add hidden layer neurons
-    for nr_hidden_layers in range(2):
+    for nr_hidden_layers in range(4):
         hidden_layer_2.append(Neuron(hidden_layer_1, last_layer))
 
-    
     #add output layer neurons fo every classification
-    last_layer.append(Neuron(hidden_layer_2, None))
-    last_layer.append(Neuron(hidden_layer_2, None))
-    last_layer.append(Neuron(hidden_layer_2, None))
+    for nr_output_layers in range(3):
+        last_layer.append(Neuron(hidden_layer_2, None))
 
-    correct_output = [1, 0, 1, 0]
+    correct_output = []
 
     itteration = 0
     samples = []
     #training the network
-    for i in range(1000):
-        data_point = random.choice(data_set.data_points)
-        sample_output = convert_to_output(data_point.classification_index, len(data_set.classifications))
-        set_input_layer(first_layer, data_point.attributes)
-        c = train_network(first_layer, [hidden_layer_1, hidden_layer_2], last_layer, 0.05, sample_output, 1)
+    c = 1
+    learning_constant = 0.1
+    i = 0
+    while i < 1000:
+        i += 1
+        last_c = c
+        c= 0
+
+        for data_point in data_set.data_points:
+            sample_output = convert_to_output(data_point.classification_index, len(data_set.classifications))
+            samples.append(sample_output)
+            set_input_layer(first_layer, data_point.attributes)
+            c += train_network(first_layer, [hidden_layer_1, hidden_layer_2], last_layer, learning_constant, sample_output, 1)
         itteration += 1
-        print(c)
+        print(c / len(data_set.data_points))
         print("==========" + str(i))
+        
+        #change learning constant based on error
+        if c / len(data_set.data_points) > 0.001:
+            if not learning_constant > 5.0:
+                learning_constant += 0.01
+        else:
+            if learning_constant > 0.04:
+                learning_constant /= 4
+        print(learning_constant)
+
+       
+
     hits = 0
 
     #getting validation results
-
-    print_weights(hidden_layer_1)
-    print_weights(hidden_layer_2)
-    print_weights(last_layer)
+    print_weights([hidden_layer_1, hidden_layer_2, last_layer])
     for i in range(len(data_set.data_points)):
         result = feed_forward_result(data_set.data_points[i].attributes, first_layer, [hidden_layer_1, hidden_layer_2], last_layer)
-        print(result)
-        # if convert_to_output() == result:
-        #     hits += 1
+        print("===================")
+        print("result:   ", result)
+        print("Expected: ", samples[i])
+        if samples[i] == result:
+            hits += 1
     print(hits / len(data_set.data_points) * 100)
         
 
